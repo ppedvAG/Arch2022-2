1d456725413af3346615b9c2c64b95609f3ea543
André <AndreR@ppedv.de>
2022-05-09 12:10:56 +0200

    Schrankbuilder

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+
+Schrank schrank = new Schrank.SchrankBuilder()
+                         .SetBöden(3)
+                         .SetTüren(6)
+                         .Create();
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloBuilder
+{
+    internal class Schrank
+    {
+        private Schrank()
+        {
+
+        }
+
+        public int AnzahlTüren { get; private set; }
+        public int AnzahlBöden { get; private set; }
+        public string Farbe { get; private set; }
+        public Oberfläche Oberfläche { get; private set; }
+
+        public class SchrankBuilder
+        {
+            Schrank newSchrank = new Schrank();
+
+           public SchrankBuilder SetTüren(int anzTüren)
+            {
+                if (anzTüren < 2 || anzTüren > 9)
+                    throw new ArgumentException("Türen sind doof");
+
+                newSchrank.AnzahlTüren = anzTüren;
+
+                return this;
+            }
+
+            public SchrankBuilder SetBöden(int anzBöden)
+            {
+                if (anzBöden < 2 || anzBöden > 9)
+                    throw new ArgumentException("Böden sind doof");
+
+                newSchrank.AnzahlBöden = anzBöden;
+
+                return this;
+            }
+
+            public Schrank Create() => newSchrank;
+        }
+
+    }
+
+    enum Oberfläche
+    {
+        Unbehandelt,
+        Gewachst,
+        Lackiert
+    }
+}
