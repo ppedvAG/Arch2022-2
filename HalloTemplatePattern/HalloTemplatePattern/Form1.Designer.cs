59115805e257f6709869f3fe055af066f8e615cd
André <AndreR@ppedv.de>
2022-05-09 14:10:37 +0200

    TemplatePattern mit WinForms

---
+++ HalloTemplatePattern/HalloTemplatePattern/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace HalloTemplatePattern
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.myButton1 = new HalloTemplatePattern.MyButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // myButton1
+            // 
+            this.myButton1.Location = new System.Drawing.Point(271, 195);
+            this.myButton1.Name = "myButton1";
+            this.myButton1.Size = new System.Drawing.Size(290, 102);
+            this.myButton1.TabIndex = 1;
+            this.myButton1.Text = "myButton1";
+            this.myButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myButton1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private MyButton myButton1;
+    }
+}
---
+++ HalloTemplatePattern/HalloTemplatePattern/MyButton.cs
@@ -0,0 +1,14 @@
+﻿namespace HalloTemplatePattern
+{
+    internal class MyButton : Button
+    {
+        int count = 0;
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            //base.OnPaint(pevent);
+            pevent.Graphics.FillRectangle(Brushes.Gold, this.ClientRectangle);
+            count++;
+            pevent.Graphics.DrawString($"count: {count}", SystemFonts.DefaultFont, Brushes.Green, this.ClientRectangle);
+        }
+    }
+}
