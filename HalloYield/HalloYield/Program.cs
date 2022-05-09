68ecbc6b0372d5436fbe38144ddcb0795f6beff9
André <AndreR@ppedv.de>
2022-05-09 14:20:11 +0200

    Hallo Yield

---
+++ HalloYield/HalloYield/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+foreach (var item in GetWoche())
+{
+    Console.WriteLine(item.ToShortDateString());
+}
+
+IReadOnlyCollection<DateTime> GetWoche___()
+{
+    var tage = new List<DateTime>();
+    tage.Add(DateTime.Now);
+    tage.Add(DateTime.Now.AddDays(1));
+    tage.Add(DateTime.Now.AddDays(2));
+    tage.Add(DateTime.Now.AddDays(3));
+    tage.Add(DateTime.Now.AddDays(4));
+    return tage;
+}
+
+
+IEnumerable<DateTime> GetWoche()
+{
+    yield return DateTime.Now;
+    yield return DateTime.Now.AddDays(1);
+    yield return DateTime.Now.AddDays(2);
+    yield return DateTime.Now.AddDays(3);
+    yield return DateTime.Now.AddDays(4);
+}
