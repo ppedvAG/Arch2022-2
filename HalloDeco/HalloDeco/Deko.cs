6309bffdb11934d8d8220c8c2025815cf20b949b
André <AndreR@ppedv.de>
2022-05-09 13:46:17 +0200

    Deko ist lecker

---
+++ HalloDeco/HalloDeco/Deko.cs
@@ -0,0 +1,53 @@
+﻿namespace HalloDeco
+{
+
+    interface IComponent
+    {
+        public string Beschreibung { get; }
+        public decimal Preise { get; }
+    }
+
+    abstract class Deko : IComponent
+    {
+        public abstract string Beschreibung { get; }
+
+        public abstract decimal Preise { get; }
+
+        protected IComponent parent;
+
+        public Deko(IComponent parent)
+        {
+            this.parent = parent;
+        }
+    }
+
+    class Käse : Deko
+    {
+        public Käse(IComponent comp) : base(comp) { }
+
+        public override string Beschreibung => parent.Beschreibung + " Käse";
+
+        public override decimal Preise => parent.Preise + 2m;
+    }
+
+    class Salami : Deko
+    {
+        public Salami(IComponent comp) : base(comp) { }
+
+        public override string Beschreibung => parent.Beschreibung + " Salami";
+
+        public override decimal Preise => parent.Preise + 4m;
+    }
+
+    public class Brot : IComponent
+    {
+        public string Beschreibung { get => "Brot"; }
+        public decimal Preise { get => 2m; }
+    }
+
+    public class Pizza : IComponent
+    {
+        public string Beschreibung { get => "Pizza"; }
+        public decimal Preise { get => 6m; }
+    }
+}
---
+++ HalloDeco/HalloDeco/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDeco;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Hello, World!");
+
+
+var p = new Käse(new Salami(new Käse(new Pizza())));
+Console.WriteLine($"Pizza: {p.Beschreibung} {p.Preise:c}");
+
+
+var b = new Käse(new Salami(new Käse(new Brot())));
+Console.WriteLine($"Brot: {b.Beschreibung} {b.Preise:c}");
