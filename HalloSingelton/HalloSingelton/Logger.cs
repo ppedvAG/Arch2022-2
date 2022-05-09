a5c21fcb020fa79f649a3c9cda021fc7fa6011f4
André <AndreR@ppedv.de>
2022-05-09 11:48:38 +0200

    Singelton jetzt auch mit ThreadSafe

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,33 @@
+﻿namespace HalloSingelton
+{
+    public class Logger
+    {
+        static Logger? _instance;
+        static object _instanceLock = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                        _instance = new Logger();
+                }
+
+                return _instance;
+                //return _instance ??= new Logger();
+            }
+        }
+
+        private Logger()
+        {
+            Log("Neue Logger instanz wurde erstellt");
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now:T}] {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,9 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+
+Parallel.For(0, 100, i =>
+{
+    Logger.Instance.Log($"Hallo logger {i}");
+});
