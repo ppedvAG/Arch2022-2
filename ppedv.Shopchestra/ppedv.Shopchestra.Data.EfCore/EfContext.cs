33f1a60d5dac36edace13155705d264631994e58
André <AndreR@ppedv.de>
2022-05-10 12:06:20 +0200

    EfCore konfig fertig

---
+++ ppedv.Shopchestra/ppedv.Shopchestra.Data.EfCore/EfContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using ppedv.Shopchestra.Model;
+
+namespace ppedv.Shopchestra.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Bestellung>? Bestellungen { get; set; }
+        public DbSet<BestellPosition>? BestellPositionen { get; set; }
+        public DbSet<Kunde>? Kunden { get; set; }
+        public DbSet<Musikinstrument>? Musikinstrumente { get; set; }
+        public DbSet<Orchester>? Orchester { get; set; }
+        public DbSet<Produkt>? Produkte { get; set; }
+
+        private string conString;
+
+        public EfContext(string conString ="Server=(localdb)\\mssqllocaldb;Database=Shopchestra_DEV;Trusted_Connection=true")
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+    }
+}
