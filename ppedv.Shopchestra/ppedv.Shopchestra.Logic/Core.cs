1af211495b5d9cac0bd67bb84d95a0ac0e350505
André <AndreR@ppedv.de>
2022-05-10 16:09:25 +0200

    Prg fertig

--- ppedv.Shopchestra/ppedv.Shopchestra.Logic/Core.cs
+++ ppedv.Shopchestra/ppedv.Shopchestra.Logic/Core.cs
@@ -1,14 +1,20 @@
-﻿using ppedv.Shopchestra.Data.EfCore;
-using ppedv.Shopchestra.Model;
+﻿using ppedv.Shopchestra.Model;
+using ppedv.Shopchestra.Model.Contracts;
 
 namespace ppedv.Shopchestra.Logic
 {
     public class Core
     {
+        public IRepository Repository { get; init; }
+
+        public Core()
+        {
+            Repository = new Data.EfCore.EfRepository();
+        }
 
         public Kunde GetVIPCustomer()
         {
-            throw new NotImplementedException();
+            return Repository.GetAll<Kunde>().OrderBy(x => x.Bestellungen.Count).FirstOrDefault();
         }
 
 
---
+++ ppedv.Shopchestra/ppedv.Shopchestra.UI.DevConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.Shopchestra.Logic;
+using ppedv.Shopchestra.Model;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("*** Shopchestra v0.1 ***");
+
+Core core = new Core();
+
+foreach (var kunde in core.Repository.GetAll<Kunde>())
+{
+    Console.WriteLine($"{kunde.Name}");
+    foreach (var b in kunde.Bestellungen)
+    {
+        Console.WriteLine($"\t{b.Bestelldatum}");
+        foreach (var pos in b.Positionen)
+        {
+            Console.WriteLine($"\t\t{pos.Menge}x {pos.Einzelpreis:c} {pos.Produkt.Bezeichnung}");
+        }
+    }
+}
+
+Console.WriteLine($"VIP: {core.GetVIPCustomer().Name}");
+
