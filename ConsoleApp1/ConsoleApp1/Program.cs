6dae073f6efe58fb4dd3bb96c18c5cf75c142cba
André <AndreR@ppedv.de>
2022-05-13 09:42:57 +0200

    CodeAnalyse mit custom Ruleset

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hallo();
+        }
+
+        private static void Hallo()
+        {
+            Console.WriteLine("AAAAA");
+            lewkfn();
+        }
+
+        private static void lewkfn()
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("");
+        }
+    }
+}
