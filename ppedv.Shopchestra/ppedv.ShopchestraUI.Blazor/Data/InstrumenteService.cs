06916a9ba1ce3107ef0324266a78a514ad4d41e6
André <AndreR@ppedv.de>
2022-05-13 10:57:33 +0200

    Coole Blazorpage mit Pusteding 🎺

---
+++ ppedv.Shopchestra/ppedv.ShopchestraUI.Blazor/Data/InstrumenteService.cs
@@ -0,0 +1,28 @@
+﻿using ppedv.Shopchestra.Model;
+using ppedv.Shopchestra.Model.Contracts;
+
+namespace ppedv.ShopchestraUI.Blazor.Data
+{
+    public class InstrumenteService
+    {
+        public IUnitOfWork Uow { get; }
+
+
+        public InstrumenteService(IUnitOfWork uow)
+        {
+            Uow = uow;
+        }
+
+        public void CreateNewPusteDing()
+        {
+            var newDing = new Musikinstrument()
+            {
+                Bezeichnung = "Pusteding",
+                Beschreibung = "Nicht umdrehen!",
+                Hersteller = "Bosch"
+            };
+            Uow.GetRepository<Musikinstrument>().Add(newDing);
+            Uow.Save();
+        }
+    }
+}
