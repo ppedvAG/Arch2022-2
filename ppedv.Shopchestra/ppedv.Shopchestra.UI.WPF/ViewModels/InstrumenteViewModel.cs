621ccb240bd110907c79977fcc5d43992ce510f8
André <AndreR@ppedv.de>
2022-05-13 12:33:54 +0200

    WPF mit billig Binding 🥜

---
+++ ppedv.Shopchestra/ppedv.Shopchestra.UI.WPF/ViewModels/InstrumenteViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ppedv.Shopchestra.Logic;
+using ppedv.Shopchestra.Model;
+using ppedv.Shopchestra.Model.Contracts;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ppedv.Shopchestra.UI.WPF.ViewModels
+{
+    public class InstrumenteViewModel /*: ObservableObject*/
+    {
+        Core core;
+
+        public Musikinstrument SelectedInstrument { get; set; }
+
+        public ObservableCollection<Musikinstrument> Musikinstrumente { get; set; } = new ObservableCollection<Musikinstrument>();
+
+        public InstrumenteViewModel()
+        {
+            core = new Core(new Data.EfCore.EfUnitOfWork());
+            LoadAll();
+        }
+
+        private void LoadAll()
+        {
+            Musikinstrumente.Clear();
+            core.UnitOfWork.GetRepository<Musikinstrument>().GetAll().ToList().ForEach(x => Musikinstrumente.Add(x));
+        }
+    }
+}
